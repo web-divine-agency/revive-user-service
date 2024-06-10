b47e2677bec70ffc54cb80ae731c928fbb262a7c
tony-tripulca <tony.tripulca@gmail.com>
2024-06-10 16:09:34 +0800

    Name changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "UserService",
+      name: "UserService.1.1",
       namespace: "skhillz-user-service",
       script: "./src/index.js",
       watch: ["./src", "./src/*.js"],
