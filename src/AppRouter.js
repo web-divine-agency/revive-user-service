d011800d213403a8dc14e8bb1370f7e43280b6e9
tony-tripulca <tony.tripulca@gmail.com>
2024-10-29 05:16:49 +0800

    Working on CORS

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import bodyParser from "body-parser";
 
 import { app } from "./Server.js";
@@ -8,8 +7,14 @@ import Controller from "./controllers/Controller.js";
 import UserController from "./controllers/UserController.js";
 import AuthController from "./controllers/AuthController.js";
 
-app.use(cors());
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://staging-portal.skhillz.com"); // Replace with your allowed origin
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const portal = express.Router();
 
