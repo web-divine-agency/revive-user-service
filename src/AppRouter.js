a594f8820140ccf9941adee8e7cd80ad63c1387a
tony-tripulca <tony.tripulca@gmail.com>
2024-10-29 06:01:43 +0800

    Working on CORS

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -10,7 +10,7 @@ import AuthController from "./controllers/AuthController.js";
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Replace with your allowed origin
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
