eeade6092ca7c25dca96149c0ec468312401be7b
tony-tripulca <tony.tripulca@gmail.com>
2025-01-21 10:30:18 +0800

    wip: migrations

--- src/database/migration.js
+++ src/database/migration.js
@@ -12,24 +12,76 @@ var mysqlClient = mysql.createPool({
 
 var exec = process.env.npm_config_exec;
 
-var enums = {
-  create_users_table: 0,
-};
+var enums = [
+  "create_users_table",
+  "create_permissions_table",
+  "create_roles_table",
+  "create_role_permissions_table",
+  "create_user_roles_table",
+]
 
 var statements = [
   // create_users_table
   `CREATE TABLE users (${[
     "id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY",
-    "type VARCHAR(255) NOT NULL",
     "first_name VARCHAR(255) NOT NULL",
     "middle_name VARCHAR(255) NULL",
     "last_name VARCHAR(255) NOT NULL",
     "gender VARCHAR(255) NULL",
+    "username VARCHAR(255) NOT NULL UNIQUE",
     "email VARCHAR(255) NOT NULL UNIQUE",
     "mobile VARCHAR(255) NULL",
     "password VARCHAR(255) NOT NULL",
     "verified_at TIMESTAMP NULL",
     "verified_at_order DOUBLE NULL",
+    "created_at TIMESTAMP NULL",
+    "created_at_order DOUBLE NULL",
+    "updated_at TIMESTAMP NULL",
+    "updated_at_order DOUBLE NULL",
+    "deleted_at TIMESTAMP NULL",
+    "deleted_at_order DOUBLE NULL",
+  ]})`,
+  // create_permissions_table
+  `CREATE TABLE permissions (${[
+    "id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY",
+    "name VARCHAR(255) NOT NULL",
+    "description TEXT NULL",
+    "created_at TIMESTAMP NULL",
+    "created_at_order DOUBLE NULL",
+    "updated_at TIMESTAMP NULL",
+    "updated_at_order DOUBLE NULL",
+    "deleted_at TIMESTAMP NULL",
+    "deleted_at_order DOUBLE NULL",
+  ]})`,
+  // create_roles_table
+  `CREATE TABLE roles (${[
+    "id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY",
+    "name VARCHAR(255) NOT NULL",
+    "description TEXT NULL",
+    "created_at TIMESTAMP NULL",
+    "created_at_order DOUBLE NULL",
+    "updated_at TIMESTAMP NULL",
+    "updated_at_order DOUBLE NULL",
+    "deleted_at TIMESTAMP NULL",
+    "deleted_at_order DOUBLE NULL",
+  ]})`,
+  // create_role_permissions_table
+  `CREATE TABLE role_permissions (${[
+    "id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY",
+    "role_id BIGINT UNSIGNED NOT NULL",
+    "permission_id BIGINT UNSIGNED NOT NULL",
+    "created_at TIMESTAMP NULL",
+    "created_at_order DOUBLE NULL",
+    "updated_at TIMESTAMP NULL",
+    "updated_at_order DOUBLE NULL",
+    "deleted_at TIMESTAMP NULL",
+    "deleted_at_order DOUBLE NULL",
+  ]})`,
+  // create_user_roles_table
+  `CREATE TABLE user_roles (${[
+    "id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY",
+    "user_id BIGINT UNSIGNED NOT NULL",
+    "role_id BIGINT UNSIGNED NOT NULL",
     "created_at TIMESTAMP NULL",
     "created_at_order DOUBLE NULL",
     "updated_at TIMESTAMP NULL",
